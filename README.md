b3788aa60ee7434c656fbfffc5526a5a54da003e
Thierry Berger <contact@thierryberger.com>
2025-08-06 10:39:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Logging with Rerun
+
+Logging with rerun is great and should be easier.
+
+Adding a dependency to rerun in libraries can be expensive maintenance-wise.
+
+This crate leverages `typed_log` to delegate rerun logging implementation to the ecosystem.
---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+pub static RERUN_LOGGER: RerunLogger = RerunLogger;
+
+use std::sync::atomic::AtomicU32;
+
+use typed_log::Loggable;
+
+pub fn register_typed_loggers() {
+    typed_log::push_log_impl(incr_main_time);
+    typed_log::push_log_impl(clear);
+}
+
+pub struct RerunLogger;
+
+static RR_CELL: std::sync::OnceLock<rerun::RecordingStream> = std::sync::OnceLock::new();
+pub static TIME: AtomicU32 = AtomicU32::new(0);
+
+pub fn init_rr() {
+    let _ = RR_CELL.get_or_init(|| {
+        rerun::RecordingStreamBuilder::new("rerun_logger")
+            .spawn()
+            .unwrap()
+    });
+    get_rr().set_duration_secs("main", 0);
+}
+
+/// Retrieves the rerun logger.
+pub fn get_rr() -> &'static rerun::RecordingStream {
+    RR_CELL.get().unwrap()
+}
+
+pub struct IncrementTime(pub u32);
+impl Loggable for IncrementTime {}
+
+pub struct Clear(pub String);
+impl Loggable for Clear {}
+
+/// Increases the time of "main" rerun timeline
+pub fn incr_main_time(increment: &IncrementTime) {
+    let time = TIME.fetch_add(increment.0, core::sync::atomic::Ordering::Relaxed);
+    get_rr().set_duration_secs("main", time + increment.0);
+}
+
+/// Clears recursively elements
+pub fn clear(clear: &Clear) {
+    get_rr()
+        .log(clear.0.as_str(), &rerun::Clear::recursive())
+        .unwrap();
+}
